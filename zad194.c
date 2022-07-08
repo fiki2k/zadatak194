d1728bdb7345a589a5e6496a23cdd904d7f82c8b
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-07-08 11:14:30 +0200

    Add files via upload

---
+++ zad194.c
@@ -0,0 +1,43 @@
+/*  Program treba recenicu napisat naopacke.  */
+
+#include <stdio.h>
+
+int main()
+{
+
+    char str[100],tekst[100];
+    int i=0,j=0;
+    printf("Unesi neku recenicu : ");
+    gets(str);
+
+// Zapocinjemo obrnut slova.
+        while(str[i]!='\0')
+                i++;
+        while(i>0)
+    {
+
+                tekst[j]=str[--i];
+                ++j;
+
+        }
+        tekst[j]='\0';
+// Zavrsavamo s obrtanjem slova.
+        printf("\nObrnuta recenica: ");
+
+    for(i=0;tekst[i]!='\0';i++)
+    {
+
+       if(tekst[i+1]==' ' || tekst[i+1]==NULL)
+       {
+
+           for(j=i;j>=0 && tekst[j]!=' ';j--)
+                        printf("%c",tekst[j]);
+
+                printf(" ");
+        }
+
+
+    }
+
+    return 0;
+}
